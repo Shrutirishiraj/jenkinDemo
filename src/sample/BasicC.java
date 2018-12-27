29c30df0b7a4c071f8a26e030fdc22aab9270c64
Shruti <shrutirishira.shruti@btes.co.in>
2018-12-27 13:52:56 +0530

    project for jenkin

---
+++ src/sample/BasicC.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class BasicC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a simple project in eclipse");
+	}
+
+}
