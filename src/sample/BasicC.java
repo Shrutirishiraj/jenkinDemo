8ee97ed5a632d80c463b95e5f37e2738ec8f5ce7
Shrutirishiraj <45587293+Shrutirishiraj@users.noreply.github.com>
2018-12-28 13:45:18 +0530

    Update BasicC.java

--- src/sample/BasicC.java
+++ src/sample/BasicC.java
@@ -4,7 +4,7 @@ public class BasicC {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is a simple project in eclipse");
+		System.out.println("This is a simple project in eclipse Sample");
 	}
 
 }
